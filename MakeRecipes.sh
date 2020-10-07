ca193db9dc908e757ed20d1cc69056c9d5354033
David Elkin-Bram <it-manager@metroeast.org>
2020-10-06 20:01:39 -0700

    updates to variable names, argument replacement, and read for parent ID

--- MakeRecipes.sh
+++ MakeRecipes.sh
@@ -11,9 +11,6 @@
 #  Inserts the App Name and AppName variants into the recipes' placeholders
 #  names the files, and is done
 #
-# parameters
-# 1: pkg recipe identifier, for updating recipes' parent
-parent_pkg_recipe_id="$1"
 
 
 # the recipe repo directory is the container for this script
@@ -29,14 +26,14 @@ patchbot_templates_dir="PatchBot_Templates"
 ## MAIN
 
 # ask for the app name
-read -p "Application Name (Patch Definition and Patch Policy names): " app_name
+read -p "Patch Software Title (Patch Definition and Patch Policy names): " patch_name
 
 # often the package is also
 # the app name w/o spaces
-pkg_name=${app_name// /}
+pkg_name=${patch_name// /}
 
 # ask for the pkg name, w/ default
-read -p "Package Name (no extension or spaces) [$pkg_name]: " this_input
+read -p "Package Name (no version, extension or spaces) [$pkg_name]: " this_input
 if [ "$this_input" != "" ]
 then
   pkg_name="$this_input"
@@ -45,11 +42,18 @@ fi
 # lowercase string of pkg name
 pkg_name_lc=$(awk '{print tolower($0)}' <<< "$pkg_name")
 
+
+# ask for the parent recipe identifier
+read -p "Parent Recipe Identifier: " parent_pkg_recipe_id
+
+
+
 echo "Using..."
-echo "App: $app_name"
+echo "App: $patch_name"
 echo "Pkg: $pkg_name"
 echo "pkg: $pkg_name_lc"
-
+echo "Parent: $parent_pkg_recipe_id"
+echo
 
 
 ## make a new directory to contain the application recipes?
@@ -67,10 +71,11 @@ else
     exit -13
   fi
 fi
+echo
 
 
 ## copy any templates into the new directory
-echo "Copying templates"
+echo "Copying templates..."
 # 
 while IFS=$'\n' read template_file
 do
@@ -78,41 +83,32 @@ do
   # the new file name
   ## replaces the template text "PackageName" with the pkg_name string
   new_file_name="${template_file/PackageName/$pkg_name}"
+  new_file_name_and_ext="${new_file_name/.templaterecipe/.recipe}"
   
   # some reporting
-  echo "Template: $recipe_container_dir/$pkg_name/$new_file_name"
+  echo "Template: $recipe_container_dir/$pkg_name/$new_file_name_and_ext"
   
   # does the new file exist?
-  if [ -e "$recipe_container_dir/$pkg_name/$new_file_name" ]
+  if [ -e "$recipe_container_dir/$pkg_name/$new_file_name_and_ext" ]
   then
     # file already there, skipping
     echo "WARN: file exists, skipping"
     #continue
   else
     # no file yet, let's make it
-    cp "$recipe_container_dir/$patchbot_templates_dir/$template_file" "$recipe_container_dir/$pkg_name/$new_file_name"
+    cp "$recipe_container_dir/$patchbot_templates_dir/$template_file" "$recipe_container_dir/$pkg_name/$new_file_name_and_ext"
     
     # replace variable placeholders with strings
-    # {Application Name} -> app_name
+    # {Application Name} -> patch_name
     # {PackageName} -> pkg_name
     # {packagename} -> pkg_name_lc
     # edit in place, the new file/copy
-    sed -e "s/{Application Name}/$app_name/g" \
+    sed -e "s/{Application Name}/$patch_name/g" \
 		-e "s/{PackageName}/$pkg_name/g" \
 		-e "s/{packagename}/$pkg_name_lc/g" \
-		-i "" "$recipe_container_dir/$pkg_name/$new_file_name"
-    
-    # replace the identifier, if provided
-    # {Pkg Recipe Identifier} -> parent_pkg_recipe_id
-    if [ "$parent_pkg_recipe_id" ]
-    then
-      sed -e "s/{Pkg Recipe Identifier}/$parent_pkg_recipe_id/g" \
-      -i "" "$recipe_container_dir/$pkg_name/$new_file_name"
-    fi
-    
-    # replace the templaterecipe extension with recipe
-    mv "$recipe_container_dir/$pkg_name/$new_file_name" "$recipe_container_dir/$pkg_name/${new_file_name/.templaterecipe/.recipe}"
-    
+		-e "s/{Pkg Recipe Identifier}/$parent_pkg_recipe_id/g" \
+		-i "" "$recipe_container_dir/$pkg_name/$new_file_name_and_ext"
+        
   fi
 done < <(
   ## list of files in the template directory
